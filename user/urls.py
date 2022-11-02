1d07d659ef6c1f86c5308afecaea1e7dfdd5dd95
BINAH LOUIS <visteen192@gmail.com>
2022-11-02 15:03:00 +0000

    password reset urls added

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = "user"
 
@@ -8,4 +9,14 @@ urlpatterns = [
     path("do_login", views.do_login, name="do_login"),
     path("logout_user", views.logout_user, name="logout_user"),
     
+    # ******************* RESET PASSWORD VIEW *****************************
+    path('password_reset', auth_views.PasswordResetView.as_view(
+        template_name='user/reset_password.html'), name='password_reset'),
+    path('password_reset_done', auth_views.PasswordResetDoneView.as_view(
+        template_name='user/reset_password_done.html'), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='user/reset_password_confirm.html'), name='password_reset_confirm'),
+    path('password_reset_complete', auth_views.PasswordResetCompleteView.as_view(
+        template_name='user/reset_password_complete.html'), name='password_reset_complete'),
+
 ]
